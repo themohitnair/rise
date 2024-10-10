373271517ea0473292a5018b182b8ba48aa200d1
Mohit <themohitnair@protonmail.com>
2024-10-10 19:45:40 +0530

    Added OG metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="shortcut icon" href="/favicon/rise.svg" type="image/svg+xml" />
+        <meta property="og:title" content="rISE - MSRIT ISE" />
+        <meta property="og:description" content="A portal for ISE students." />
+        <meta property="og:image" content="/images/preview.png" />
+        <meta property="og:url" content="https://riserit.vercel.app" />
       </head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
