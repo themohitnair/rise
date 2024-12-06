e496c5dac12ce3e545eebb7f8931a3d163e19544
Mohit <themohitnair@protonmail.com>
2024-12-06 14:42:59 +0530

    Added dynamic og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,19 +16,20 @@ export const metadata: Metadata = {
     },
 }
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children, }: { children: React.ReactNode; }) {
     return (
         <html lang="en">
-            <body className="font-fira">
-                <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-                <Navbar />
-                {children}
-                </ThemeProvider>
-            </body>
+        <body className="font-fira">
+            <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            >
+            <Navbar />
+            {children}
+            </ThemeProvider>
+        </body>
         </html>
     );
 }
