e623160a73a6973f94cbdcd06b49ff326c40f6a3
Mohit <themohitnair@protonmail.com>
2024-12-25 22:30:43 +0530

    Made navbar backdrop blurred with transparent bg

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -59,7 +59,7 @@ export default function Navbar() {
     ]
 
     return (
-        <header className="sticky top-0 px-4 md:px-6 py-4 bg-background z-50">
+        <header className="sticky top-0 px-4 md:px-6 py-4 bg-transparent z-50 backdrop-blur">
             <div className="flex items-center justify-between">
                 <div className="font-extrabold text-2xl md:text-3xl">
                     <Link href="/" className="text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-pink-500 to-red-500">
