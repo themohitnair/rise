8a16e636efffa7843b9d389bc1d56b7a22181110
Mohit <themohitnair@protonmail.com>
2025-01-05 16:27:11 +0530

    Added resource to list and programmatically appended year to all displayed links

--- src/app/resources/Resource.tsx
+++ src/app/resources/Resource.tsx
@@ -9,13 +9,13 @@ export interface ResourceProps {
     semester: number
 }
 
-const Resource: React.FC<ResourceProps> = ({ name, link }) => {
+const Resource: React.FC<ResourceProps> = ({ name, link, year }) => {
     return (
         <Card className="w-full mb-2">
             <a href={link} className="block" target="_blank" rel="noopener noreferrer">
                 <CardHeader className="p-4">
                 <CardTitle className="text-sm font-medium flex justify-between items-center">
-                    <span>{name}</span>
+                    <span>{name} ({year})</span>
                     <ExternalLink className="h-4 w-4" />
                 </CardTitle>
                 </CardHeader>
--- src/app/resources/ResourceList.ts
+++ src/app/resources/ResourceList.ts
@@ -32,157 +32,157 @@ const allResources: ResourceProps[] = [
         semester: 4
     },
     {
-        name: "Programming Language Courses (2022)",
+        name: "Programming Language Courses",
         link: "https://drive.google.com/drive/folders/16H_ZocnhRtMqhhPzeezEirPV2XraF62E?usp=sharing",
         year: 2022,
         semester: 1
     },
     {
-        name: "Engineering Science Courses (2022)",
+        name: "Engineering Science Courses",
         link: "https://drive.google.com/drive/folders/1mAYYFscw5ZJARG8l5V5JZBRTawueHwMo?usp=sharing",
         year: 2022, 
         semester: 1
     },
     {
-        name: "Engineering Science Courses (2022)",
+        name: "Engineering Science Courses",
         link: "https://drive.google.com/drive/folders/1mAYYFscw5ZJARG8l5V5JZBRTawueHwMo?usp=sharing",
         year: 2022, 
         semester: 2
     },
     {
-        name: "Emerging Technology Courses (2022)",
+        name: "Emerging Technology Courses",
         link: "https://drive.google.com/drive/folders/1uOc4mVNJ6A6SU1KQJ3jM3GuBTy8Ksa7J?usp=sharing",
         year: 2022,
         semester: 2
     },
     {
-        name: "Core Subjects (2022)",
+        name: "Core Subjects",
         link: "https://drive.google.com/drive/folders/1VH0n2vks5ccCXVX3JZeI3H7qhs3kFecv?usp=sharing",
         year: 2022,
         semester: 1
     },
     {
-        name: "Core Subjects (2022)",
+        name: "Core Subjects",
         link: "https://drive.google.com/drive/folders/1V3gUXrs7_RTYdjHhK4HTGEvrE-sjaSZ6?usp=sharing",
         year: 2022,
         semester: 2
     },
     {
-        name: "Core Subjects (2023)",
+        name: "Core Subjects",
         link: "https://drive.google.com/drive/folders/1xIKvspacb9dPCnqXq5GcyYpDySYWtY30?usp=sharing",
         year: 2023,
         semester: 2
     },
     {
-        name: "Core Subjects (2023)",
+        name: "Core Subjects",
         link: "https://drive.google.com/drive/folders/1Q2hzPD9GT7zp3Gx_na6sAl5cojxVQfWf?usp=sharing",
         year: 2023,
         semester: 1
     },
     {
-        name: "Data Structures Lab Repository (2023)",
+        name: "Data Structures Lab Repository",
         link: "https://github.com/themohitnair/DSLab",
         year: 2023,
         semester: 3
     },
     {
-        name: "Digital Systems and Computer Orgranization Lab Repository (2023)",
+        name: "Digital Systems and Computer Orgranization Lab Repository",
         link: "https://github.com/themohitnair/DCOLab",
         year: 2023, 
         semester: 3
     },
     {
-        name: "UNIX and Shell Lab Repository (2023)",
+        name: "UNIX and Shell Lab Repository",
         link: "https://github.com/themohitnair/UNIXLab",
         year: 2023,
         semester: 3
     },
     {
-        name: "Programming with C Lab Repository (2022)",
+        name: "Programming with C Lab Repository",
         link: "https://github.com/themohitnair/PPC18",
         year: 2022,
         semester: 1
     },
     {
-        name: "PLC Java Lab Repository (2022)",
+        name: "PLC Java Lab Repository",
         link: "https://github.com/themohitnair/PLCJava",
         year: 2022,
         semester: 1
     },
     {
-        name: "Advanced Java Lab Repository (2023)",
+        name: "Advanced Java Lab Repository",
         link: "https://github.com/themohitnair/AdvJava",
         year: 2023, 
         semester: 4
     },
     {
-        name: "Microcontrollers Lab Repository (2023)",
+        name: "Microcontrollers Lab Repository",
         link: "https://github.com/themohitnair/MCLab",
         year: 2023, 
         semester: 4
     },
     {
-        name: "Design and Analysis of Algorithms Lab Repository (2023)",
+        name: "Design and Analysis of Algorithms Lab Repository",
         link: "https://github.com/themohitnair/DAALab",
         year: 2023,
         semester: 4
     },
     {
-        name: "Programming Language Courses (2023)",
+        name: "Programming Language Courses",
         link: "https://drive.google.com/drive/folders/1fz5vA0n8HsWeOEtglFNIVJZ75OchPFvF?usp=drive_link",
         year: 2023,
         semester: 1
     },
     {
-        name: "Engineering Science Courses (2023)",
+        name: "Engineering Science Courses",
         link: "https://drive.google.com/drive/folders/1e0Lpi1I_gl0o-mvLJIEoQvc7e8mXZgEL?usp=drive_link",
         year: 2023, 
         semester: 1
     },
     {
-        name: "Engineering Science Courses (2023)",
+        name: "Engineering Science Courses",
         link: "https://drive.google.com/drive/folders/1e0Lpi1I_gl0o-mvLJIEoQvc7e8mXZgEL?usp=drive_link",
         year: 2023, 
         semester: 2
     },
     {
-        name: "Emerging Technology Courses (2023)",
+        name: "Emerging Technology Courses",
         link: "https://drive.google.com/drive/folders/1S8QjtcYkm9TkplfBfkkzX8aecVyRY20V?usp=drive_link",
         year: 2023,
         semester: 2
     },
     {
-        name: "PYQ Papers (2023)",
+        name: "PYQ Papers",
         link: "https://drive.google.com/drive/folders/1AwjK2nb2tQM4pZ4wN5i--5FabCrgLBTF?usp=drive_link",
         year: 2023,
         semester: 1
     },
     {
-        name: "PYQ Papers (2023)",
+        name: "PYQ Papers",
         link: "https://drive.google.com/drive/folders/1FlrR3ByGSKOSNWbvrDf59MwhE8HjHm7o?usp=drive_link",
         year: 2023,
         semester: 2
     },
     {
-        name: "Physics Lab Essentials (2022)",
+        name: "Physics Lab Essentials",
         link: "https://drive.google.com/drive/folders/1Rd8T-2tbMd0VvjHpNFv-5tukjXl8ghN5?usp=drive_link",
         year: 2022,
         semester: 1
     },
     {
-        name: "CIE and SEE QPs (2023)",
+        name: "CIE and SEE QPs",
         link: "https://drive.google.com/drive/folders/1St7oM_2r3PJinNu0hxto_R9rsnUh4sCP?usp=drive_link",
         year: 2023,
         semester: 4
     },
     {
-        name: "CIE and SEE QPs (2023)",
+        name: "CIE and SEE QPs",
         link: "https://drive.google.com/drive/folders/1whIn3Re7ZwN_AogzopOjGTn8Nx-AuYI9?usp=drive_link",
         year: 2023,
         semester: 3
     },
     {
-        name: "Subjectwise Notes (2024)",
+        name: "Subjectwise Notes",
         link: "https://drive.google.com/drive/folders/1q7X0uemHMGFlCcVFSgJEakE84f4E_oXb?usp=sharing",
         year: 2024,
         semester: 5
@@ -190,6 +190,12 @@ const allResources: ResourceProps[] = [
     {
         name: "Textbooks",
         link: "https://drive.google.com/drive/folders/1o_GtFa_QRpYZrMWpBx6bK0sS73BE5_6i?usp=sharing",
+        year: 2024,
+        semester: 5
+    },
+    {
+        name: "Data Visualization Lab Repository",
+        link: "https://github.com/themohitnair/DVLab",
         year: 2024,
         semester: 5
     }
